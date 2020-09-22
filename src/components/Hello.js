fe2b0608ce75738a6dad4e6cbacebb9166997d94
ShuwenLi <shuwenli89@126.com>
2020-09-22 15:03:59 +0800

    [Shuwen] add hello world component.

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Hello.css';
+
+const names = ['Shuwen', 'Yang', 'Xuanyi'];
+
+const Hello = () => names.map(name => 
+    <h1 className='greet badge badge-info' key={name} style={{border: '1px solid black'}} >
+        Hello {name}
+    </h1>
+    ); 
+
+export default Hello;
